af8fa688086cc6f8c88096fe0ac5be0f386ac0af
queennduji <40236859+queennduji@users.noreply.github.com>
2020-04-16 13:46:42 -0400

    Exercise class added. Notifypropertychanged implemented in all models

---
+++ CalorieCounter/CalorieCounter/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CalorieCounter.ViewModels
+{
+    public class Exercise : INotifyPropertyChanged
+    {
+        public string ExerciseType { get; set; }
+
+        public double CalorieBurnt { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- CalorieCounter/CalorieCounter/ViewModels/MainWindowViewModel.cs
+++ CalorieCounter/CalorieCounter/ViewModels/MainWindowViewModel.cs
@@ -10,7 +10,7 @@ namespace CalorieCounter.ViewModels
 {
     public class MainWindowViewModel
     {
-        public ObservableCollection<FoodCalorie> FoodCalories { get; set; } = new ObservableCollection<FoodCalorie>
+        public ObservableCollection<FoodCalorie> BreakFastCalories { get; set; } = new ObservableCollection<FoodCalorie>
         {
             new FoodCalorie {Food = "Beans", Calorie = 1800},
             new FoodCalorie {Food = "Rice", Calorie = 1900},
@@ -19,8 +19,39 @@ namespace CalorieCounter.ViewModels
         };
 
 
-        //public ObservableCollection<FoodCalorie> FoodCalories { get; set; }
+        public ObservableCollection<FoodCalorie> LunchCalories { get; set; } = new ObservableCollection<FoodCalorie>
+        {
+            new FoodCalorie {Food = "Beans", Calorie = 1800},
+            new FoodCalorie {Food = "Rice", Calorie = 1900},
+            new FoodCalorie {Food = "Yam", Calorie = 1700},
+            new FoodCalorie {Food = "Parsnips", Calorie = 1600}
+        };
 
+        public ObservableCollection<FoodCalorie> DinnerCalories { get; set; } = new ObservableCollection<FoodCalorie>
+        {
+            new FoodCalorie {Food = "Beans", Calorie = 1800},
+            new FoodCalorie {Food = "Rice", Calorie = 1900},
+            new FoodCalorie {Food = "Yam", Calorie = 1700},
+            new FoodCalorie {Food = "Parsnips", Calorie = 1600}
+        };
+
+        public ObservableCollection<FoodCalorie> SnacksCalories { get; set; } = new ObservableCollection<FoodCalorie>
+        {
+            new FoodCalorie {Food = "Beans", Calorie = 1800},
+            new FoodCalorie {Food = "Rice", Calorie = 1900},
+            new FoodCalorie {Food = "Yam", Calorie = 1700},
+            new FoodCalorie {Food = "Parsnips", Calorie = 1600}
+        };
+
+        public ObservableCollection<Exercise> ExerciseCalories { get; set; } = new ObservableCollection<Exercise>
+        {
+            new Exercise {ExerciseType = "Cardio", CalorieBurnt = 1800},
+            new Exercise {ExerciseType = "HIT", CalorieBurnt = 1800},
+            new Exercise {ExerciseType = "Cardio", CalorieBurnt = 1800},
+            new Exercise {ExerciseType = "HIT", CalorieBurnt = 1800}
+        };
+
+        public ObservableCollection<double> WaterCalories { get; set; } = new ObservableCollection<double> { 5,6,5};
 
 
     }
